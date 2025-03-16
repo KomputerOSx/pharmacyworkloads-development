cb809cc43cbce093599cdea6bc5251529b2be288
KomputerOSx <ramyarburhan26@gmail.com>
2025-03-16 15:05:19 +0000

    Corrected Footer not sticking to bottom

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -1,6 +1,7 @@
 // app/(main)/layout.tsx
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
+import "../../styles/main-layout.css";
 
 export default function MainLayout({
     children,
