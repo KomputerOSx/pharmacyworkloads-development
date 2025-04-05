3b6e82149d9a8e994affafb958190b987e3e31d2
KomputerOSx <ramyarburhan26@gmail.com>
2025-04-05 11:48:24 +0100

    HOOK are actually created now

--- src/components/team-switcher.tsx
+++ src/components/team-switcher.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import * as React from "react";
-import { ChevronsUpDown, House, Plus } from "lucide-react";
+import { ChevronsUpDown, House } from "lucide-react";
 
 import {
     DropdownMenu,
--- src/types/hospTypes.ts
+++ src/types/hospTypes.ts
@@ -1,4 +1,4 @@
-import { DocumentReference, Timestamp } from "firebase/firestore";
+import { Timestamp } from "firebase/firestore";
 
 export type Hosp = {
     id: string;
